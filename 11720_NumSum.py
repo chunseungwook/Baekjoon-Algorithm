57a99497a3df1e4147ebac5d40e5b4b1e8f8a4a3
chunseungwook <cswguy3574@naver.com>
2021-12-10 19:47:39 +0900

    11720_NumSum
    보기
    숫자의 합

---
+++ 11720_NumSum.py
@@ -0,0 +1,10 @@
+import sys
+Num = int(sys.stdin.readline())
+#list_Num = list(map(int,sys.stdin.readline() for i in range(Num)))
+numbers = input()
+sum = 0
+for n in numbers :
+    sum +=int(n)
+#    l = 0
+print(sum)
+
